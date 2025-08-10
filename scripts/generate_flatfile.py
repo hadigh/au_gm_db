440a360ad530e93f3180fd9a83f5aa584aff2d8a
PCUser <pcuser@localhost.localdomain>
2025-08-10 15:15:36 +1000

    adding revised generate_flatfile.py and flatfiles for both passed and failed

--- scripts/generate_flatfile.py
+++ scripts/generate_flatfile.py
@@ -1,6 +1,8 @@
 import os
 from gmprocess.io.asdf.stream_workspace import StreamWorkspace
 import pandas as pd
+from obspy.geodetics import locations2degrees, gps2dist_azimuth
+
 
 
 """
@@ -16,7 +18,7 @@ Note:
 # Define the base directory
 data_directory = "../gmprocess_projects/data"
 output_file = "../outputs/flatfile.csv"
-
+failed_output_file = "../outputs/failed_flatfile.csv"
 # df data initiation
 recs_ids = []
 recs_hpcs = []
@@ -24,6 +26,14 @@ recs_dists = []
 eqs_ids = []
 eqs_mags = []
 eqs_deps = []
+
+# df_fail data initiation
+fail_recs_ids = []
+fail_recs_dists = []
+fail_recs_reason = []
+fail_eqs_ids = []
+fail_eqs_mags = []
+fail_eqs_deps = []
 
 # Iterate through all directories within the data directory
 for root, dirs, files in os.walk(data_directory):
@@ -53,7 +63,26 @@ for root, dirs, files in os.walk(data_directory):
                             recs_ids.append(tr.id)
                             recs_dists.append(tr.get_parameter('fit_spectra')['epi_dist']) # change this later?
                             recs_hpcs.append(tr.get_parameter('corner_frequencies')['highpass'])
-                    # breakpoint()   
+                    else:
+                        # get epicentral distance for the stream
+                        sta_lat = stream[0].stats.coordinates.latitude
+                        sta_lon = stream[0].stats.coordinates.longitude
+                        distance_m, az12, az21 = gps2dist_azimuth(eve.latitude, eve.longitude,
+                                                                   sta_lat, sta_lon) 
+                        # Convert meters to kilometers
+                        distance_km = distance_m / 1000
+                        
+                        for tr in stream:
+                            fail_recs_ids.append(tr.id)
+                            fail_recs_dists.append(distance_km)
+                            fail_eqs_ids.append(event_id)
+                            fail_eqs_mags.append(eve.magnitude)
+                            fail_eqs_deps.append(eve.depth_km)
+                            if tr.passed:
+                                msg = 'Passed'
+                            else:
+                                msg = tr.get_parameter('failure')['reason']
+                            fail_recs_reason.append(msg) 
 
             except Exception as e:
                 print(f"Error processing {workspace_path}: {e}")
@@ -71,14 +100,26 @@ data = {
     'depth': eqs_deps
 }
 
+fail_data = {
+    'earthquake_id': fail_eqs_ids,
+    'record_id': fail_recs_ids,
+    'fail_reason': fail_recs_reason,
+    'distance': fail_recs_dists,
+    'magnitude': fail_eqs_mags,
+    'depth': fail_eqs_deps
+}
+
 # Create the DataFrame from the dictionary
 df = pd.DataFrame(data)
 
+# Create the DataFrame from the dictionary for failed records!
+df_fail = pd.DataFrame(fail_data)
+
 # Print the DataFrame to see the result
-print(df)
 df.to_csv(output_file)
             
-
-            
+# Print the DataFrame to see the result
+df_fail.to_csv(failed_output_file)
+                  
 
           
